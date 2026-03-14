bff53475777193f221996df769202d782da2d133
zaenal-blip <arif4321456789@gmail.com>
2026-03-14 13:30:59 +0700

    refactor

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ import { createMasterDataRouter } from "./modules/master-data/master-data.router
 import { createCheckingRouter } from "./modules/checking/checking.router.js";
 import { ReflectionRouter } from "./modules/reflection/reflection.router.js";
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 export class App {
   app: express.Express;
@@ -26,7 +26,7 @@ export class App {
 
   private configure = () => {
     this.app.use(cors({
-      origin: "http://localhost:5173",
+      origin: process.env.FRONTEND_URL || "http://localhost:5173",
       credentials: true
     }));
     this.app.use(express.json({ limit: "50mb" }));
