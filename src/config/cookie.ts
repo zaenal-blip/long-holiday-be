e38acf45f80db23b0f28eae9233b2790b6ce912d
zaenal-blip <arif4321456789@gmail.com>
2026-03-14 10:06:00 +0700

    add cookie.ts

---
+++ src/config/cookie.ts
@@ -0,0 +1,8 @@
+import { CookieOptions } from "express";
+
+export const cookieOptions: CookieOptions = {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    path: "/",
+};
